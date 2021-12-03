b4539f30f7f372e9e562ba425858915dd9a4310d
drgn <62900543+dnrecording@users.noreply.github.com>
2021-12-03 21:44:26 +0700

    Create main.go

---
+++ guessing-game/auth-sql/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"auth-sql/routes"
+
+	"auth-sql/database"
+
+	"github.com/gofiber/fiber/v2"
+
+	"github.com/gofiber/fiber/v2/middleware/cors"
+)
+
+func main() {
+	database.Connect()
+
+	app := fiber.New()
+
+	app.Use(cors.New(cors.Config{
+		AllowCredentials: true,
+	}))
+
+	routes.Setup(app)
+
+	app.Listen(":8000")
+}
